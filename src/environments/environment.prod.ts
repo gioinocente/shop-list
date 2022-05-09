891a24e399e906db440b32d32e993ba91409069a
Giovane Winck <55024577+gioinocente@users.noreply.github.com>
2022-05-09 19:21:07 -0300

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCqLTjhNigVriSkNwvmPP79Zao2id0MlR0",
+    authDomain: "gioinocente-shoping-list.firebaseapp.com",
+    databaseURL: "https://gioinocente-shoping-list-default-rtdb.firebaseio.com",
+    projectId: "gioinocente-shoping-list",
+    storageBucket: "gioinocente-shoping-list.appspot.com",
+    messagingSenderId: "353772373684",
+    appId: "1:353772373684:web:896dc0a258e356389179e1",
+    measurementId: "G-8G9PYDL4CR"
+  }
+};
